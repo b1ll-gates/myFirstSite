dcb235c3b13a4ed6a81e9eca65172703d2580ccc
Your Name <you@example.com>
2022-02-02 08:46:18 +0000

    Updates

---
+++ precache-manifest.5ff4b3faca33f2498124f0c73e9b2553.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cb89d5078400963cd853a686cea02d9",
+    "url": "/myFirstSite/index.html"
+  },
+  {
+    "revision": "39d9d3aeeca88e53924e",
+    "url": "/myFirstSite/static/css/main.03973b52.chunk.css"
+  },
+  {
+    "revision": "b24c351d0f45242a431d",
+    "url": "/myFirstSite/static/js/2.1bc1a0ce.chunk.js"
+  },
+  {
+    "revision": "e677167b3e984c661c6c",
+    "url": "/myFirstSite/static/js/3.fd3f3dda.chunk.js"
+  },
+  {
+    "revision": "39d9d3aeeca88e53924e",
+    "url": "/myFirstSite/static/js/main.d2d6068a.chunk.js"
+  },
+  {
+    "revision": "b922a3c9de616d549ef4",
+    "url": "/myFirstSite/static/js/runtime~main.d5d4c004.js"
+  },
+  {
+    "revision": "9f0bee9f606125e05a5eb07d751b095c",
+    "url": "/myFirstSite/static/media/NwBTC.9f0bee9f.png"
+  },
+  {
+    "revision": "2ad283feb98e36a61d8da35df9503739",
+    "url": "/myFirstSite/static/media/clock.2ad283fe.svg"
+  },
+  {
+    "revision": "6a5ccf9611132e63df247e90caeb8129",
+    "url": "/myFirstSite/static/media/info.6a5ccf96.png"
+  },
+  {
+    "revision": "678db5cd945b520ea6e039a6af7ee8fd",
+    "url": "/myFirstSite/static/media/injection.678db5cd.svg"
+  },
+  {
+    "revision": "19b52f0e88c256958d375e7ff15f6e49",
+    "url": "/myFirstSite/static/media/leave.19b52f0e.png"
+  },
+  {
+    "revision": "6f6fd09867d8b7dee8a1be2ef32a94e6",
+    "url": "/myFirstSite/static/media/link.6f6fd098.png"
+  },
+  {
+    "revision": "ef121af311c606b87334f969423f28b3",
+    "url": "/myFirstSite/static/media/sperm.ef121af3.svg"
+  },
+  {
+    "revision": "42630ee975362d1106683929b739b450",
+    "url": "/myFirstSite/static/media/sperm_die.42630ee9.svg"
+  },
+  {
+    "revision": "fc63f4b48971df9547f75a2f19897ca7",
+    "url": "/myFirstSite/static/media/sperm_left.fc63f4b4.svg"
+  },
+  {
+    "revision": "8d7d0d367e47e3b7bea593095791d6e3",
+    "url": "/myFirstSite/static/media/sperm_right.8d7d0d36.svg"
+  }
+]);
